bee5539cd5faa82342f9a16bf49f7496608e9b89
Matej Sajgal <matej@sajgal.com>
2015-01-07 14:45:30 +1100

    webhook edit

--- webhook.php
+++ webhook.php
@@ -1,5 +1,9 @@
 <?php
 
-file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . time() . '.txt', $_POST);
-echo 'Posted';
+if ( $_POST['payload'] ) {
+    file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . time() . '.txt', $_POST['payload']);
+    echo 'Posted';
+    exit;
+}
 
+echo 'not Posted';
