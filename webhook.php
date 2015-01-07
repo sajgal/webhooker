2b5161ed713b03a9c7f15bd0778abd3a763cc356
Matej Sajgal <matej@sajgal.com>
2015-01-07 14:34:34 +1100

    webhooker edit

--- webhook.php
+++ webhook.php
@@ -1,8 +1,9 @@
 <?php
 
 if (isset($_POST) && !empty($_POST)) {
-    file_put_contents(time(), $_POST);
+    file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . time() . '.txt', $_POST);
     echo 'Posted';
 }
 
 echo '...';
+
