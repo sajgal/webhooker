bcdb6769a70b5232bc204218a1f0420967b8e086
matej.sajgal <matej@sajgal.com>
2015-01-07 21:10:47 +1100

    webhook commands

--- src/hook.php
+++ src/hook.php
@@ -2,22 +2,73 @@
 
 class Hook
 {
+    private $rawPayload;
+    private $githubSecretKey;
+    private $branch = 'master';
+    private $repository = 'origin';
 
+    public function __construct($githubSecretKey)
+    {
+        $this->rawPayload = file_get_contents('php://input');
+        $this->githubSecretKey = $githubSecretKey;
+    }
+
+    public function validateSignature()
+    {
+        if (!array_key_exists('HTTP_X_HUB_SIGNATURE', $_SERVER)) {
+            throw new Exception('Missing X-Hub-Signature header.');
+        }
+
+        return 'sha1=' . hash_hmac('sha1', $this->rawPayload, $this->githubSecretKey, false) === $_SERVER['HTTP_X_HUB_SIGNATURE'];
+    }
+
+    public function getPayload()
+    {
+        return json_decode($this->rawPayload);
+    }
+
+    public function pull()
+    {
+        $branch = $this->getBranch();
+        $repository = $this->getRepository();
+        $cwd = getcwd();
+
+        $commands = array();
+        $commands[] = sprintf('cd %s', $cwd);
+        $commands[] = sprintf('git pull %s %s', $repository, $branch);
+
+        return shell_exec(implode(';', $commands));
+    }
+
+    /**
+     * @param string $branch
+     */
+    public function setBranch($branch)
+    {
+        $this->branch = $branch;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBranch()
+    {
+        return $this->branch;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRepository()
+    {
+        return $this->repository;
+    }
+
+    /**
+     * @param string $repository
+     */
+    public function setRepository($repository)
+    {
+        $this->repository = $repository;
+    }
 }
-
-
-//$post = file_get_contents('php://input');
-//
-//if (!empty($post)) {
-//
-//    try {
-//        $payload = json_decode($post);
-//
-//        file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . time() . '.txt', $payload->pusher->email);
-//        echo 'Posted ----- ';
-//        exit;
-//    } catch (Exception $e) {
-//        echo $e->getMessage();
-//        exit;
-//    }
-//}
--- webhook_entry.php
+++ webhook_entry.php
@@ -7,7 +7,10 @@ include_once('vendor/autoload.php');
 Debugger::enable(Debugger::DEVELOPMENT);
 
 
+$hook = new Hook('6O<<sYq0037H4i#O9o?90<6@4FrOo`');
 
-$hook = new Hook();
+if(!$hook->validateSignature()) {
+    throw new Exception('Signature not valid!');
+}
 
-Debugger::dump($_SERVER);
+echo $hook->pull();
