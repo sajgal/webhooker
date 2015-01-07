9943a9950d688d3898eac0473edfd95dfb390a36
Matej Sajgal <matej@sajgal.com>
2015-01-07 15:32:58 +1100

    webhook post issue

--- webhook.php
+++ webhook.php
@@ -2,8 +2,8 @@
 
 $post = file_gets_contents('php://input');
 
-if (!empty($post['payload'])) {
-    $payload = json_decode($post['payload']);
+if (!empty($post)) {
+    $payload = json_decode($post);
 
     file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . time() . '.txt', $payload);
     echo 'Posted ----- ';
